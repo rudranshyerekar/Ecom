109e01224a667903460cd427dc813c631f3eea20
rudranshyerekar <rudranshyerekar45@gmail>
2026-03-15 11:43:15 +0530

    first commit

---
+++ ecommerce-backend/src/main/java/com/ecommerce/ecommerce_backend/model/Product.java
@@ -0,0 +1,24 @@
+package com.ecommerce.ecommerce_backend.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private double price;
+
+    private int stock;
+}
