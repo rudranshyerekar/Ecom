12974b1b56eca6185d5e3c6712d3198c106c461f
Harshal Moon <2020bcs065S@sggs.ac.in>
2026-06-19 00:11:14 +0530

    Created security-config for permitting endpoints

--- ecommerce-backend/src/main/java/com/ecommerce/ecommerce_backend/service/AuthService.java
+++ ecommerce-backend/src/main/java/com/ecommerce/ecommerce_backend/service/AuthService.java
@@ -2,16 +2,23 @@ package com.ecommerce.ecommerce_backend.service;
 
 import com.ecommerce.ecommerce_backend.model.User;
 import com.ecommerce.ecommerce_backend.repository.UserRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+@AllArgsConstructor
 @Service
 public class AuthService {
 
-    @Autowired
-    private UserRepository userRepository;
+
+    private final UserRepository userRepository;
+
+
+    private final PasswordEncoder passwordEncoder;
 
     public User register(User user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         user.setRole("CUSTOMER");
         return userRepository.save(user);
     }
